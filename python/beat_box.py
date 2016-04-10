98e41eb4eebcb862d95b60780228cd191229cda5
gautamjeyaraman <gautamjeyaraman@gmail.com>
2016-04-10 08:09:05 -0400

    Adding new mp3

--- python/beat_box.py
+++ python/beat_box.py
@@ -76,7 +76,7 @@ def generate(FILENAME, dt=None):
     midi_convert.to_audio(SF2, midi_path, INTER)
     sound1 = AudioSegment.from_mp3(INTER+"/"+FILENAME+".wav")
     sound1 = sound1 + 15
-    sound2 = AudioSegment.from_mp3("soundfont/beats.mp3")
+    sound2 = AudioSegment.from_mp3("soundfont/beats"+str(choice(range(1,9)))+".mp3")
     sound2 = sound2 - 10
     output = sound1.overlay(sound2, loop=True)
     output.export(OUTPUT+"/"+FILENAME+".mp3", format="mp3",tags={'artist': 'HackerPack', 'album': 'Stocks', 'comments': FILENAME +' stock data'})
